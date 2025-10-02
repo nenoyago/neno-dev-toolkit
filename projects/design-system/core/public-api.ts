682d35b42bde80f1ecd6b25544d1af4a35536448
Yago Neno <developer.neno@gmail.com>
2025-10-02 18:39:03 -0300

    feat: ✨  create icon component

--- projects/design-system/core/public-api.ts
+++ projects/design-system/core/public-api.ts
@@ -2,3 +2,5 @@ export * from './theme/default-theme';
 export * from './theme/theme-config';
 export * from './theme/theme.provider';
 export * from './theme/theme.token';
+
+export * from './icon/provide-icon-loader';
