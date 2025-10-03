a48c58b5518d829897aeefb0f2dc50317484e3d6
Yago Neno <developer.neno@gmail.com>
2025-10-03 13:43:14 -0300

    feat: ✨  dadas

--- projects/shared-utilities/pipes/src/file-size/file-size.pipe.ts
+++ projects/shared-utilities/pipes/src/file-size/file-size.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import { formatBytes } from '@common-utilities';
+import { formatBytes } from '@nenoyago/common-utilities';
 
 @Pipe({
   name: 'fileSize',
