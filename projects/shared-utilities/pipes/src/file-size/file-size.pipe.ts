38f28e00e993e474aedc0358c54681aa8f62eeee
Yago Neno <developer.neno@gmail.com>
2025-10-03 13:12:56 -0300

    feat: ✨  common utilities library

--- projects/shared-utilities/pipes/src/file-size/file-size.pipe.ts
+++ projects/shared-utilities/pipes/src/file-size/file-size.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import { formatBytes } from '@common-utils';
+import { formatBytes } from '@common-utilities';
 
 @Pipe({
   name: 'fileSize',
